a7c169ee675d0bc16cccbdf062c979f1a70ff43b
sanduniWijesinghe <nipunisandunikaw@gmail.com>
2025-04-05 22:43:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function printName(name) {
+    console.log("Your name is: " + name);
+}
+printName("Sanduni Wijesinghe");
---
+++ index.ts
@@ -0,0 +1,6 @@
+function printName(name: string): void {
+    console.log("Your name is: " + name);
+}
+
+printName("Sanduni Wijesinghe");
+
